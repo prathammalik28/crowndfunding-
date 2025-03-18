c366e7c3a316a1ec434969a003b1153292e68e27
prathammalik28 <malikpratham004@gmail.com>
2025-03-18 18:37:04 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+//  mongoUrl : "mongodb+srv://shubhranshu1924patel:UctDP38hVA6OLhXZ@cluster0.iyvlom3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+ mongoUrl : "mongodb+srv://shubhranshu1924patel:Mc7QNBRrgWnP5ovp@cluster0.jstdn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+
+
+ jwt_secret : "37ry9wru9j8rehfu",
+ NODE_ENV:'production'
+};
+
